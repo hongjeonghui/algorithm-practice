e6bf21d05d2ef6c4b2f6f3308bd613b632122616
hongjeonghui <jngpop123@gmail.com>
2025-09-06 21:25:05 +0900

    Programmers: 입문_자릿수더하기

--- Programmers/자릿수 더하기.py
+++ Programmers/자릿수 더하기.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = 0
-    n = str(n)
+    n = str(n)  # 숫자는 하나씩 뽑지 못하니까 문자열로 전환
     for i in n:
-        answer += int(i)
+        answer += int(i)  # 다시 정수형으로 변환
     
     return answer
