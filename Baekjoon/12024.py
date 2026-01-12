36753049c50f6c50e6085400e59b10f33e52cd94
hongjeonghui <jngpop123@gmail.com>
2026-01-12 21:04:41 +0900

    Baekjoon: 12024 사각형 찾기

--- Baekjoon/12024.py
+++ Baekjoon/12024.py
@@ -1,35 +1,18 @@
-import sys
-sys.stdin = open("input.txt")
-
 N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-
+A = [list(map(int, input().split())) for _ in range(N)]
 
 cnt = 0
+
 for i in range(N):
-    for j in range(N):
-        if arr[i][j] == 1:
-            width = N  # 가능한 최대 가로 길이
+    for k in range(N):
+        if i == k:
+            continue
 
-            for x in range(i, N):
-                if arr[x][j] == 0:
-                    break
+        common = 0
+        for j in range(N):
+            if A[i][j] == 1 and A[k][j] == 1:
+                common += 1
 
-                # 현재 줄에서 가로로 몇 칸인지 1인지 확인
-                current_width = 0
-                for y in range(j, N):
-                    if arr[x][y] == 1:
-                        current_width += 1
-                    else:
-                        break
-
-                # 가로 길이는 최소값 유지
-                width = min(width, current_width)
-
-                # 현재 높이에서 가능한 사각형 개수 추가
-                cnt += width
+        cnt += common * (common - 1)
 
 print(cnt)
-
-
-
